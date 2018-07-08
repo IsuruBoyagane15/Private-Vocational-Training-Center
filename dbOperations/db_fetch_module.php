6621faa59bede936aabb97208c2491dde6d2fd84
ThaminduDilshan <thamindudill071@gmail.com>
2018-07-08 17:03:50 +0530

    Some database inequality fixed with lecture profile

--- dbOperations/db_fetch_module.php
+++ dbOperations/db_fetch_module.php
@@ -1,21 +1,40 @@
 <!-- Adding relevent module codes to the left panel -->
 
 <?php
-  $connection = mysqli_connect("localhost", "root", "", "courses_details");
+
+  //select relavent course
+  $connection = mysqli_connect("localhost", "root", "", "staff");
   $id = $_POST["id"];
-  $output = '';
-  $query = "SELECT module_id,module_name FROM module_details WHERE lecturer_id='$id'";
+  $query = "SELECT staff_id,course FROM staff_details WHERE staff_id='$id'";
   $result = mysqli_query($connection, $query);
+  $row = mysqli_fetch_array($result);
+  $course = (string)$row[1];
+  mysqli_close($connection);
 
-  while( $row = mysqli_fetch_array($result) ){
-    $output .= ' <li><a href="#" data-id=" ';
-    $output .= (string)$row[0];
-    $output .= ' " class="mod_link"> ';
-    $output .= (string)$row[1];
-    $output .= ' </a></li> ';
+  //get course id
+  $connection = mysqli_connect("localhost", "root", "", "courses_details");
+  $query = "SELECT COURSE_NAME,COURSE_ID FROM course_details WHERE COURSE_NAME='$course'";
+  $result = mysqli_query($connection, $query);
+  $row = mysqli_fetch_array($result);
+  $course_id = $row[1];
+  mysqli_close($connection);
+
+  //select module id's from relevent course table
+  $connection = mysqli_connect("localhost", "root", "", "courses_details");
+  $query = "SELECT * FROM {$course_id}";
+  $result = mysqli_query($connection, $query);
+  $output = '';
+  if( !empty($course_id) ){
+    while( $row = mysqli_fetch_array($result) ){
+      $output .= ' <li><a href="#" data-id=" ';
+      $output .= (string)$row[0];
+      $output .= ' " class="mod_link"> ';
+      $output .= (string)$row[0];
+      $output .= ' </a></li> ';
+    }
   }
-
   mysqli_close($connection);
 
   echo $output;
+
 ?>
